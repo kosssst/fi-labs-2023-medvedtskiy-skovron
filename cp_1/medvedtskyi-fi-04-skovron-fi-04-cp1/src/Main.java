8e81d900e86ee0f7e15d6a992b1f6129e2522e3e
LTX <48835997+Lin-Rin@users.noreply.github.com>
2023-02-23 21:53:46 +0200

    test struct

--- cp_1/medvedtskyi-fi-04-skovron-fi-04-cp1/src/Main.java
+++ cp_1/medvedtskyi-fi-04-skovron-fi-04-cp1/src/Main.java
@@ -1,5 +1,8 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! this is lab 1");
+        Tools app = new Tools();
+        app.run();
     }
+
 }
---
+++ cp_1/medvedtskyi-fi-04-skovron-fi-04-cp1/src/Tools.java
@@ -0,0 +1,25 @@
+public class Tools {
+
+    private final char[] alphabet = null;
+
+    Tools() {
+
+    }
+
+    public void run() {
+
+    }
+
+    private void filter(){
+
+    }
+
+    private long calLetter(){
+        throw new UnsupportedOperationException();
+    }
+
+    private long calBiGram(){
+        throw new UnsupportedOperationException();
+    }
+
+}
