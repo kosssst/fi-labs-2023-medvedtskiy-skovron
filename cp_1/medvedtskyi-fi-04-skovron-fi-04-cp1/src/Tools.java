ad797c7329669474d003676b71b43a761598d58f
LTX <48835997+Lin-Rin@users.noreply.github.com>
2023-03-10 20:58:27 +0200

    filter & readFile

--- cp_1/medvedtskyi-fi-04-skovron-fi-04-cp1/src/Tools.java
+++ cp_1/medvedtskyi-fi-04-skovron-fi-04-cp1/src/Tools.java
@@ -1,43 +1,78 @@
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
+
+// Война и мир
+
+// calBigram 2 methods
+// H1, H2
+
+// bug # symbols
 
 public class Tools {
 
-    private final char[] alphabet = null;
+    private final InputStream srcFile = new FileInputStream("cp_1/medvedtskyi-fi-04-skovron-fi-04-cp1/src/sourceText.txt");
+    private final InputStream clearTxt;
 
-    Tools() {
+    Tools() throws IOException {
+        filter();
+        clearTxt = new FileInputStream("cp_1/medvedtskyi-fi-04-skovron-fi-04-cp1/src/nFile.txt");
+    }
+
+    public void run() throws IOException {
+        var map = calLetter(readFromInputStream(clearTxt).replaceAll("\n", ""));
+
+        map.forEach((key, value) -> System.out.println(key + ":" + value));
+    }
+
+    private void filter() throws IOException {
+        String temp = readFromInputStream(srcFile);
+
+        temp = temp.replaceAll("ё", "e").replaceAll("ъ", "ь");
+        temp = temp.replaceAll("[^^а-яА-Я]", "");
+        temp = temp.toLowerCase();
+
+        try {
+            File file = new File("cp_1/medvedtskyi-fi-04-skovron-fi-04-cp1/src/nFile.txt");
+            FileWriter writer = new FileWriter(file);
+            writer.write(temp);
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
 
     }
 
-    public void run() {
-        System.out.println(calLetter());
-    }
-
-    private void filter(){
-
-    }
-
-    private Map calLetter(){
-        // throw new UnsupportedOperationException();
-        Map<Character, Integer> letters = new HashMap<>();
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter text: ");
-        String input = scanner.nextLine();
-        for (int i = 0; i < input.length(); i++){
-            char c = input.charAt(i);
-            if (!letters.containsKey(c)){
-                letters.put(c,1);
-            }else{
-                int n = letters.get(c);
-                letters.put(c, (n+1));
+    private String readFromInputStream(InputStream inputStream) throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
             }
         }
-        scanner.close();
+        return resultStringBuilder.toString();
+    }
+
+    private Map<Character, Integer> calLetter(String input) {
+        Map<Character, Integer> letters = new HashMap<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (!letters.containsKey(c)) {
+                letters.put(c, 1);
+            } else {
+                int n = letters.get(c);
+                letters.put(c, (n + 1));
+            }
+        }
+
         return letters;
     }
 
-    private long calBiGram(){
+    private long calBiGram(String input){
         throw new UnsupportedOperationException();
     }
 
