1c96cc16bf285fb4fdfd964b24cfd831e9c27d64
Kostia <47629852+kosssst@users.noreply.github.com>
2023-04-02 20:46:07 +0300

    added calculation for key length

--- cp_2/medvedtskyi-fi-04-skovron-fi-04-cp2/src/Tools.java
+++ cp_2/medvedtskyi-fi-04-skovron-fi-04-cp2/src/Tools.java
@@ -5,14 +5,15 @@ import java.util.Random;
 
 public class Tools {
     private static final String symbols = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
-    private static final String FILE_PATH = "cp_2/medvedtskyi-fi-04-skovron-fi-04-cp2/src/windows_license.txt";
+    private static final String FILE_PATH_TO_ENCODE = "cp_2/medvedtskyi-fi-04-skovron-fi-04-cp2/src/windows_license.txt";
+    private static final String FILE_PATH_TO_DECODE = "cp_2/medvedtskyi-fi-04-skovron-fi-04-cp2/src/to_decode.txt";
     public Tools(){
 
     }
 
     public void run() throws IOException {
-        PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
-        System.setOut(out);
+//        PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
+//        System.setOut(out);
 
         String key2 = generateKey(2);
         String key3 = generateKey(3);
@@ -20,46 +21,50 @@ public class Tools {
         String key5 = generateKey(5);
         String key10 = generateKey(10);
 
-        String openText = filter();
+        String openText = filter(FILE_PATH_TO_ENCODE);
         String encodedByKey2 = encode(openText, key2);
         String encodedByKey3 = encode(openText, key3);
         String encodedByKey4 = encode(openText, key4);
         String encodedByKey5 = encode(openText, key5);
         String encodedByKey10 = encode(openText, key10);
 
-        System.out.println(key2);
-        System.out.println("****************************************************");
-        System.out.println(key3);
-        System.out.println("****************************************************");
-        System.out.println(key4);
-        System.out.println("****************************************************");
-        System.out.println(key5);
-        System.out.println("****************************************************");
-        System.out.println(key10);
-        System.out.println("****************************************************");
-        System.out.println("I = " + calculateI(openText));
-        System.out.println(openText);
-        System.out.println("****************************************************");
-        System.out.println("I = " + calculateI(encodedByKey2));
-        System.out.println(encodedByKey2);
-        System.out.println("****************************************************");
-        System.out.println("I = " + calculateI(encodedByKey3));
-        System.out.println(encodedByKey3);
-        System.out.println("****************************************************");
-        System.out.println("I = " + calculateI(encodedByKey4));
-        System.out.println(encodedByKey4);
-        System.out.println("****************************************************");
-        System.out.println("I = " + calculateI(encodedByKey5));
-        System.out.println(encodedByKey5);
-        System.out.println("****************************************************");
-        System.out.println("I = " + calculateI(encodedByKey10));
-        System.out.println(encodedByKey10);
+        String EncodedText = filter(FILE_PATH_TO_DECODE);
 
-        out.close();
+//        System.out.println(key2);
+//        System.out.println("****************************************************");
+//        System.out.println(key3);
+//        System.out.println("****************************************************");
+//        System.out.println(key4);
+//        System.out.println("****************************************************");
+//        System.out.println(key5);
+//        System.out.println("****************************************************");
+//        System.out.println(key10);
+//        System.out.println("****************************************************");
+//        System.out.println("I = " + calculateI(openText));
+//        System.out.println(openText);
+//        System.out.println("****************************************************");
+//        System.out.println("I = " + calculateI(encodedByKey2));
+//        System.out.println(encodedByKey2);
+//        System.out.println("****************************************************");
+//        System.out.println("I = " + calculateI(encodedByKey3));
+//        System.out.println(encodedByKey3);
+//        System.out.println("****************************************************");
+//        System.out.println("I = " + calculateI(encodedByKey4));
+//        System.out.println(encodedByKey4);
+//        System.out.println("****************************************************");
+//        System.out.println("I = " + calculateI(encodedByKey5));
+//        System.out.println(encodedByKey5);
+//        System.out.println("****************************************************");
+//        System.out.println("I = " + calculateI(encodedByKey10));
+//        System.out.println(encodedByKey10);
+
+        System.out.println("Length of key:");
+        calculateKeyLength(EncodedText).forEach((key, value) -> System.out.println(key + ": " + value));
+//        out.close();
     }
 
-    private String filter() throws IOException {
-        InputStream srcFile = new FileInputStream(Tools.FILE_PATH);
+    private String filter(String filePath) throws IOException {
+        InputStream srcFile = new FileInputStream(filePath);
         String temp = readFromInputStream(srcFile);
 
         temp = temp.replaceAll("ё", "e");
@@ -84,7 +89,7 @@ public class Tools {
         return resultStringBuilder.toString();
     }
 
-    private String encode(String text, String key){
+    private String encode(String text, String key) {
         int indexInKey = 0;
         StringBuilder builder = new StringBuilder(text.length());
 
@@ -97,7 +102,7 @@ public class Tools {
         return builder.toString();
     }
 
-    private String generateKey(Integer length){
+    private String generateKey(Integer length) {
         Random random = new Random();
         StringBuilder builder = new StringBuilder(length);
 
@@ -109,7 +114,7 @@ public class Tools {
         return builder.toString();
     }
 
-    private float calculateI(String text){
+    private float calculateI(String text) {
         Map<Character, Integer> letters = calculateLetters(text);
         float sum = 0;
         for (Integer value : letters.values()) {
@@ -118,7 +123,7 @@ public class Tools {
         return (float) (sum / (text.length() * (text.length() - 1)));
     }
 
-    private Map<Character, Integer> calculateLetters(String text){
+    private Map<Character, Integer> calculateLetters(String text) {
         Map<Character, Integer> letters = new HashMap<>();
 
         for (int i = 0; i < text.length(); i++) {
@@ -132,5 +137,26 @@ public class Tools {
         }
 
         return letters;
+    }
+
+    private Map<Integer, Float> calculateKeyLength(String text) {
+        Map<Integer, Float> r = new HashMap<>();
+        for (int i = 2; i <= 30; i++) {
+            float sum = 0;
+            StringBuilder[] builders = new StringBuilder[i];
+            for (int j = 0; j < i; j++) {
+                builders[j] = new StringBuilder((text.length() / i));
+            }
+            int k = 0;
+            while(k < text.length()) {
+                builders[(k % i)].append(text.charAt(k));
+                k++;
+            }
+            for (int j = 0; j < i; j++) {
+                sum += calculateI(builders[j].toString());
+            }
+            r.put(i, (sum / i));
+        }
+        return r;
     }
 }
