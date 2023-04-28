3da74a948c644044044958256a7068ab0ac4f991
Kostia <47629852+kosssst@users.noreply.github.com>
2023-04-28 13:57:43 +0300

    added getting text from file

--- cp_3/medvedtskyi-fi-04-skovron-fi-04-cp3/src/Tools.java
+++ cp_3/medvedtskyi-fi-04-skovron-fi-04-cp3/src/Tools.java
@@ -1,11 +1,13 @@
+import java.io.*;
 import java.util.Map;
 
 public class Tools {
 
-    private static final String alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
+    private static final String ALPHABET = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
+    private static final String FILE_PATH = "cp_3/medvedtskyi-fi-04-skovron-fi-04-cp3/src/to_decode.txt";
 
-    public void run(){
-        throw new UnsupportedOperationException("to do main code");
+    public void run() throws IOException{
+        System.out.println(getText(FILE_PATH));
     }
 
     private int gcd(int a, int b){
@@ -24,8 +26,22 @@ public class Tools {
         throw new UnsupportedOperationException("to do bigrams calculation");
     }
 
-    private String getText(String filepath){
-        throw new UnsupportedOperationException("to do reading from file");
+    private String getText(String filepath) throws IOException {
+        InputStream file = new FileInputStream(filepath);
+        StringBuilder resultStringBuilder = new StringBuilder();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(file))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+
+        String temp = resultStringBuilder.toString();
+        temp = temp.replaceAll("\n", "");
+
+        return temp;
     }
 
     private String decodeText(String text, String key){
