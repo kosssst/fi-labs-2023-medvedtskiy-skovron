2d1431f7f104583079b75cf57318ce2098e9c1e1
Kostia <47629852+kosssst@users.noreply.github.com>
2023-04-28 13:41:49 +0300

    added lab3 struct

--- cp_3/medvedtskyi-fi-04-skovron-fi-04-cp3/src/Main.java
+++ cp_3/medvedtskyi-fi-04-skovron-fi-04-cp3/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world3!");
+        Tools app = new Tools();
+        app.run();
     }
 }
---
+++ cp_3/medvedtskyi-fi-04-skovron-fi-04-cp3/src/Tools.java
@@ -0,0 +1,34 @@
+import java.util.Map;
+
+public class Tools {
+
+    private static final String alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
+
+    public void run(){
+        throw new UnsupportedOperationException("to do main code");
+    }
+
+    private int gcd(int a, int b){
+        throw new UnsupportedOperationException("to do gcd calculation");
+    }
+
+    private int reverse(int a, int b){
+        throw new UnsupportedOperationException("to do finding reverse element");
+    }
+
+    private int[] solveLinearComparisons(int[] a){
+        throw new UnsupportedOperationException("to do solving linear comparisons");
+    }
+
+    private Map<Character, Integer> calculateBigrams(String text){
+        throw new UnsupportedOperationException("to do bigrams calculation");
+    }
+
+    private String getText(String filepath){
+        throw new UnsupportedOperationException("to do reading from file");
+    }
+
+    private String decodeText(String text, String key){
+        throw new UnsupportedOperationException("to do text decoding");
+    }
+}
